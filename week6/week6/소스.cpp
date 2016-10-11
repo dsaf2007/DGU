f70f80dcf98108950157c8c9806a04785cf31d1b
dsaf2007 <dsaf2007@naver.com>
2016-10-11 13:14:12 +0900

    problem2

--- week6/week6/소스.cpp
+++ week6/week6/소스.cpp
@@ -7,11 +7,31 @@ void print(const T_Class& i)
 {
 	std::cout << i << std::endl;
 }
+template <class T>
+class vector2d
+{
+public:
+	T x_, y_;
+};
 
 void main()
 {
-	print(1);
+	/*print(1);
 	print(1.1f);
-	print("hello world");
+	print("hello world");*/
+	vector2d<int> my_vector; 
+	vector2d<double> my_vector2;
+	vector2d<float> my_vector3;
+
+	my_vector.x_ = 1;
+	my_vector.y_ = 2;
+
+	my_vector2.x_ = 2.0;
+	my_vector2.y_ = 3.0;
+
+	my_vector3.x_ = 1.0f;
+	my_vector3.y_ = 1.0f;
+
+
 
 }
